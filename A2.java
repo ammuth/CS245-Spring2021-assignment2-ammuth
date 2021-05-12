e765e1b13cef1b5233edac7b2e48656b8103684e
Annaliese Muth <61072717+ammuth@users.noreply.github.com>
2021-05-11 19:39:14 -0700

    Add files via upload

--- A2.java
+++ A2.java
@@ -21,31 +21,31 @@ import java.util.NoSuchElementException;
 
 public class A2 {
 	
-	public static class Graph
-	{
+	public static class Graph {
+
 		private HashMap<String, ArrayList<String>> adjHash;
 
-		//Constructor
+
 		public Graph(){
 			adjHash = new HashMap<String, ArrayList<String>>();
 		}
 
-		//Adds a vertex to the graph.
+
 		public void addVertex(String vertex){
 			ArrayList<String> list = new ArrayList<String>();
-			adjHash.putIfAbsent(vertex, list);		//Will not add a vertex that already exists.
+			adjHash.putIfAbsent(vertex, list);
 		}
 
-		//Adds an edge between two vertices.
+
 		public void addEdge(String v1, String v2){
 			ArrayList<String> v1list = adjHash.get(v1);
-			if (!v1list.contains(v2)){				//Will not add an edge that already exists.
+			if (!v1list.contains(v2)){
 				v1list.add(v2);
 			}
 			adjHash.replace(v1, v1list);
 		}
 
-		//Returns true if there's an edge between two vertices.
+
 		public boolean edgeExists(String v1, String v2){
 			ArrayList<String> v1edges = adjHash.get(v1);
 			if (v1edges.contains(v2)){
@@ -54,23 +54,23 @@ public class A2 {
 			return false;
 		}
 
-		//Returns the number of elements in the hashtable.
+
 		public int getSize(){
 			return adjHash.size();
 		}
 
-		//Returns a list of all the edges a vertex is connected to.
+
 		public ArrayList<String> neighbors(String vertex){
 			return adjHash.get(vertex);
 		}
 
-		//Returns a list of all the vertices in the graph.
+
 		public ArrayList<String> getVertices(){
 			ArrayList<String> keys = new ArrayList<String>(adjHash.keySet());
 			return keys;
 		}
 
-		//Prints out all the key-value pairs of the hashtable.
+
 		public void printGraph(){
 			for (String key : adjHash.keySet()){
 				String value = adjHash.get(key).toString();
@@ -80,22 +80,22 @@ public class A2 {
 		}
 	}
 
-	//Circular queue implementation using arrays.
-	public static class ArrayQueue<T>
-	{
+
+	public static class ArrayQueue<T> {
+
 		Object[] tempArray = new Object[10];
 		T[] arr;
 		int head;
 		int tail;
 
-		//Constructor
+
 		public ArrayQueue(){
 			arr = (T[]) tempArray;
 			head = 0;
 			tail = 0;
 		}
 		
-		//Checks to see if queue is empty.
+
 		public boolean empty(){
 			if (head == tail){
 				return true;
@@ -103,80 +103,80 @@ public class A2 {
 			return false;
 		}
 
-		//Removes and returns the first element in the queue.
+
 		public T dequeue(){
 			if (empty()){
 				throw new NoSuchElementException();
 			}
-			T temp = arr[head];		//Holds element at head to be returned.
-			head = (head + 1) % arr.length;		//Moves value of head to next index in queue.
+			T temp = arr[head];
+			head = (head + 1) % arr.length;
 			return temp;
 		}
 
-		//Adds item to the end of the queue.
-		public void enqueue(T item)
-		{
-			if ((tail + 1) % arr.length == head){	//If array is too small, tail will reach the head and we must make a bigger circular array.
-				grow_array();						//We have (tail + 1) to keep at least one slot open in queue.
+
+		public void enqueue(T item) {
+
+			if ((tail + 1) % arr.length == head){
+				grow_array();
 			}
-			arr[tail++] = item;		//Copies item to the tail, and increments tail.
-			tail = tail % arr.length;	//Re-initializes tail to fit within the circular array.
+			arr[tail++] = item;
+			tail = tail % arr.length;
 		}
 
-		//Makes the array bigger to fit more elements.
+
 		protected void grow_array(){
-			Object[] tempObject = new Object[arr.length * 2];	//Creates a temporary array that's 2x bigger.
+			Object[] tempObject = new Object[arr.length * 2];
 			T[] temp = (T[]) tempObject;
-			for (int i = 0; i < arr.length; i++){		//Iterates through each slot in queue.
-				temp[i] = arr[(head + i) % arr.length];		//Copies each item in queue to temp array, starting at head.
+			for (int i = 0; i < arr.length; i++){
+				temp[i] = arr[(head + i) % arr.length];
 			}
-			tail = arr.length - 1;		//Re-initializes tail for new array.
-			arr = temp;			//Assigns new larger array to queue.
-			head = 0;		//Re-initializes head.
+			tail = arr.length - 1;
+			arr = temp;
+			head = 0;
 		}			
 	}
 
-	//BFS algorithm that outputs the shortest path between two vertices in an unweighted graph.
-	private static ArrayList<String> breadthFS(Graph graph, String actor1, String actor2)
-	{
-		HashMap<String, Boolean> visited = new HashMap<String, Boolean>();		//Keeps track of which vertices have been taken care of.
-		HashMap<String, String> previous = new HashMap<String, String>();		//Keeps track of the vertex that precedes the current one during BFS.
-		ArrayList<String> path = new ArrayList<String>();						//Records the path between the source vertex to the destination vertex.
-		ArrayQueue<String> queue = new ArrayQueue<String>();					//A to-do list of the vertices we will need to visit next as we go through BFS.
 
-		String current = actor1;		//Adds the source vertex to the queue and visited table.
+	private static ArrayList<String> breadthFS(Graph graph, String actor1, String actor2) {
+
+		HashMap<String, Boolean> visited = new HashMap<String, Boolean>();
+		HashMap<String, String> previous = new HashMap<String, String>();
+		ArrayList<String> path = new ArrayList<String>();
+		ArrayQueue<String> queue = new ArrayQueue<String>();
+
+		String current = actor1;
 		queue.enqueue(current);
 		visited.put(current, true);
 
-		while (!queue.empty())
-		{
-			current = queue.dequeue();						//Removes the first item from the queue.
-			if (current.equalsIgnoreCase(actor2))			//While loop continues until we find the destination vertex.
-			{
+		while (!queue.empty()) {
+
+			current = queue.dequeue();
+			if (current.equalsIgnoreCase(actor2)) {
+
 				break;
 			}
-			else
-			{
-				for (String actor : graph.neighbors(current))		//Loops through all of the current vertex's edges, and adds them to queue and the visited table.
-				{
-					if (visited.get(actor) == null)
-					{
+			else {
+
+				for (String actor : graph.neighbors(current)) {
+
+					if (visited.get(actor) == null) {
+
 						queue.enqueue(actor);
 						visited.put(actor, true);
-						previous.put(actor, current);				//Current is documented as the vertex that precedes each of its edges.
+						previous.put(actor, current);
 					}
 				}
 			}
 		}
-		if (!current.equalsIgnoreCase(actor2))				//If we don't find the destination after going through the loop, then no path exits and we can quit.
-		{
+		if (!current.equalsIgnoreCase(actor2)) {
+
 			System.out.println("No such path exists between Actor 1 and Actor 2.");
 			return null;
 		}
 		
-		String m = actor2;			//Starting from the destination vertex, we continuously add the previous element to the final path.
-		while (m != null)
-		{
+		String m = actor2;
+		while (m != null) {
+
 			path.add(m);
 			m = previous.get(m);
 		}
@@ -192,10 +192,7 @@ public class A2 {
         	Reader reader = new FileReader(args[0]);
         	CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT);
         	JSONParser jsonParser = new JSONParser();
-          
-           
-                
-                //int movies = 0;
+
             
                 
             boolean firstround = true;
@@ -239,10 +236,10 @@ public class A2 {
 
         	
             
-            //csvParser.close();
+
        
         
-        	// User Input:
+
         
      	Scanner scan = new Scanner(System.in);
      	System.out.println();
@@ -252,8 +249,7 @@ public class A2 {
      	System.out.print("Actor 2 name: ");
      	String actor2 = scan.nextLine();
      		
-     	//Correctly capitalizes the user input by looping through all the vertices in the graph and matching it with the input.
-     	//Also checks to make sure the given names are valid actors.
+
      	boolean actor1found = false;
      	boolean actor2found = false;
      	ArrayList<String> vertices = graph.getVertices();
@@ -272,10 +268,10 @@ public class A2 {
      			return;
      		}
 
-     		//Performs a BFS on the graph from the first to second actor.
+
      		ArrayList<String> path = breadthFS(graph, actor1, actor2);
      		
-     		//Prints the final results.
+
      		System.out.println("------------------------------------------------");
      		System.out.println("Path from " + actor1 + " to " + actor2 + ": ");
      		System.out.print(path.get(path.size() - 1) + " ");
